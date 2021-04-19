25ffc2dff76983ab952f709b45807150a2a42f40
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-19 23:43:03 +0300

    Advanced functions with events

--- script8.js
+++ script8.js
@@ -1,12 +1,24 @@
 let add = document.querySelectorAll(".nav-link");
+let tabPane = document.querySelectorAll(".tab-pane");
+let chooseTab;
+
+
 
 
 add.forEach(el => {
     el.addEventListener("click", ev => {
-      add.forEach(i => {
+        chooseTab = ev.target.id;
+        add.forEach(i => {
           i.className = "nav-link";
-      })
-      console.log("ev");
+      });
+      tabPane.forEach(iTab => {
+        iTab.className = "tab-pane";
+      if(iTab.getAttribute("aria-labelledby") ==  chooseTab){
+        console.log(iTab);
+        iTab.setAttribute("class" , "tab-pane active");
+      }
+    });
+     console.log("event");
       console.log(ev);
       ev.target.setAttribute("class", "nav-link active");
     });
