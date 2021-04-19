90cbf94cf52d96dbe2b42779c58cff8b7ae16492
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-04-19 23:44:26 +0300

    change now chek

---
+++ script7.js
@@ -0,0 +1,30 @@
+let me = document.getElementById('click_on');
+
+me.addEventListener("click",(click_on) =>  {
+console.log('click!');
+});
+
+let events = document.getElementById('click_on');
+
+events.addEventListener('click', click_on => {
+let myP = document.createElement('p');
+myP.innerHTML = 'hello world';
+document.body.appendChild(myP);
+click_on.stopPropagation();
+console.log(click_on);
+});
+
+let  title = document.getElementById('start');
+
+
+let k = document.createTextNode('create-text');
+k.innerHTML = 'create-text';
+title.appendChild(k);
+
+let A = document.createElement('h1');
+A.innerHTML = "i am learn git now";
+document.body.appendChild(A);
+
+A.style.width = "500px";
+A.style.backgroundColor = "green";
+A.style.height = "500px";
